4fdc5ffec39af40221272d560baa30cc64ecd211
Sumama Waheed <sumamawaheed1@gmail.com>
2016-04-09 17:25:35 -0500

    signup button working

--- www/templates/signup/signupCtrl.js
+++ www/templates/signup/signupCtrl.js
@@ -1,23 +1,43 @@
 myControllers.controller(
 	'SignupCtrl', 
 
-	function($scope,AuthService) {
+	function($scope,AuthService,$state) {
 
 		console.log("SignupCtrl") 
 		$scope.auth = AuthService;
 
 
-		$scope.signUpData = {};
+		$scope.signUpData = {
+			firstName : "male1",
+			lastName  : "male1",
+			age		  : 10,
+			email     : "male1@gmail.com",
+			password  : "asdf",
+			masjid    : "IACC",
+			gender    : "M",
+			interests : {
+				basketball : true,
+				soccer	   : true,
+				football   : false,
+				badminton  : false
+			},
+			hafidh	  : true
+			
+		};
 
 
-		$scope.logIn = function() { 
-			AuthService.logIn($scope.logInData,function(err,res){
+		$scope.signUp = function() { 
+			AuthService.signUp($scope.signUpData,function(err,res){
 				console.log("err: " + err);
 				console.log("res: " + res);
+				
+				if(!err){
+					$state.transitionTo('tab.dash',{location:"replace"});
+				}
 			});
 
 		}; 
- 
+
 
 
 
